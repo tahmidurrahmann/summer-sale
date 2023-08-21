a7580953938b18643d968030d6ac4db72b997cd4
tahmidurrahmann <mirazurrahmannn@gmail.com>
2023-08-21 23:27:20 +0600

    added the last part

--- js/index.js
+++ js/index.js
@@ -8,27 +8,34 @@ function calculationSummerClick(btn){
     appendContainer.appendChild(p);
     const amount = parseFloat(btn.childNodes[3].childNodes[5].innerText.split(' ')[0]);
     sum = parseFloat(sum) + parseFloat(amount);
+    const totalPrice = document.getElementById('total-price')
     const firstTotal = (document.getElementById('total-price').innerText) = sum;
     const makePurchase = document.getElementById('make-purchase');
+    
+    
+    const inputCoupon = document.getElementById('input-coupon');
+    const inputCouponString = inputCoupon.value;
+    const paymentTotal = document.getElementById('payment-total');
+    const totalDiscount = document.getElementById('total-discount');
+    const remain = sum * 0.8;
+    const discount = sum * 0.2;
+    const applyBtn = document.getElementById('apply-btn').addEventListener('click',function(){
+        if(sum > 200 && inputCouponString === 'SUM200'){
+            applyBtn.removeAttribute('disabled');
+            
+            totalDiscount.innerText = discount;
+            paymentTotal.innerText = remain;
+           }
+        else{
+        applyBtn.setAttribute('disabled', 'true')
+    }
+})
     if(sum > 1){
-        makePurchase.removeAttribute('disabled')
+        makePurchase.removeAttribute('disabled');
     }
     else{
         makePurchase.setAttribute('disabled', 'true')
     }
-    const inputCoupon = document.getElementById('input-coupon');
-    const inputCouponString = inputCoupon.value;
-    const paymentTotal = document.getElementById('payment-total');
-    const discount = sum * 0.2;
-    const remain = sum * 0.8;
-    const totalDiscount = document.getElementById('total-discount');
-    const applyBtn = document.getElementById('apply-btn');
-    if(sum >= 200 && inputCouponString === 'SUM200'){
-        applyBtn.removeAttribute('disabled');
-        totalDiscount.innerText = discount;
-        paymentTotal.innerText = remain;
-    }
-    else{
-        applyBtn.setAttribute('disabled', 'true')
-    }
+   
+
 }
