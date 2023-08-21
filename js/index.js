1bead4f9d8c101e4664326a62929b5244bedead4
tahmidurrahmann <mirazurrahmannn@gmail.com>
2023-08-21 21:50:59 +0600

    added the second version

--- js/index.js
+++ js/index.js
@@ -9,8 +9,20 @@ function calculationSummerClick(btn){
     const amount = parseFloat(btn.childNodes[3].childNodes[5].innerText.split(' ')[0]);
     sum = parseFloat(sum) + parseFloat(amount);
     const firstTotal = (document.getElementById('total-price').innerText) = sum;
-    return firstTotal;
+    const totalDiscount = document.getElementById('total-discount');
+    const paymentTotal = document.getElementById('payment-total');
+    const remain = firstTotal* 0.8;
+    const discount = firstTotal * 0.2;
+    if(firstTotal > 200){
+        totalDiscount.innerText = discount.toFixed(2);
+        paymentTotal.innerText = remain.toFixed(2);
+    }
 }
-function purchaseBtn(){
-    
+
+function applyBtn(){
+    const inputCoupon = parseFloat(document.getElementById('input-coupon').value);
+    const discount = firstTotal * 0.2;
+    if(inputCoupon === 'SELL200'){
+
+    }
 }
